20b38288f697661c4b593ee7102ec6013f8ef4eb
emerson-feral <emerson_feral@hotmail.com>
2025-02-19 19:48:44 -0300

    Test dummy function

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -7,5 +7,5 @@ test("Should return the expected sum", () => {
   const num2 = 2;
 
   const result = sum(num1, num2);
-  assert.strictEqual(num1 + 5, result);
+  assert.strictEqual(num1 + num2, result);
 });
